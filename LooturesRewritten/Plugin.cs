1ca06441b4fe30de2fd3bc04208561ea72f40f8b
Emma <153868115+GayPotatoEmma@users.noreply.github.com>
2026-07-18 02:42:09 +0200

    initial setup

--- LooturesRewritten/Plugin.cs
+++ LooturesRewritten/Plugin.cs
@@ -1,12 +1,12 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using LooturesRewritten.Windows;
 
-namespace SamplePlugin;
+namespace LooturesRewritten;
 
 public sealed class Plugin : IDalamudPlugin
 {
@@ -22,7 +22,7 @@ public sealed class Plugin : IDalamudPlugin
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("LooturesRewritten");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
